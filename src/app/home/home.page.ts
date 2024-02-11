8ff7a3d1b9c85907908ecca9a7c518e21b88c1b6
Walrus98 <samuelecalugi@gmail.com>
2024-02-11 22:14:38 +0100

    ionic app

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, Input, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonicModule, FormsModule],
 })
 export class HomePage {
-  constructor() {}
+
+  username?: string; 
+  password?: string; 
+
+  constructor() { }
+
+  login() {
+    console.log('Username:', this.username);
+    console.log('Password:', this.password);
+  }
 }
